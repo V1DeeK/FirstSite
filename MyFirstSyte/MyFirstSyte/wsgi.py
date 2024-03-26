42acca6606a49264901191b29702742db21f84d6
V1DeeK <97947424+V1DeeK@users.noreply.github.com>
2024-03-26 23:09:19 +0300

    first commit

---
+++ MyFirstSyte/MyFirstSyte/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MyFirstSyte project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstSyte.settings')
+
+application = get_wsgi_application()
